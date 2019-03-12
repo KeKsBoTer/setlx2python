c921111514aae7330a33aac1ccdd023525ae7a3b
Simon Niedermayr <14186588+KeKsBoTer@users.noreply.github.com>
2019-03-12 19:11:27 +0100

    added a option to pass code as cli argument

--- setlx2python/cli.py
+++ setlx2python/cli.py
@@ -2,16 +2,18 @@
 setlx2python
  
 Usage:
-  setlx2python <file>
+  setlx2python [-c] <file>
   setlx2python -h | --help
   setlx2python --version
  
 Options:
   -h --help                         Show this screen.
   --version                         Show version.
+  -c                                File is code
  
 Examples:
   setlx2python hello.stlx
+  setlx2python -c "print(5!);"
  
 Help:
   For help using this tool, please open an issue on the Github repository:
@@ -23,14 +25,19 @@ import sys
 from docopt import docopt
 from astor import to_source, string_repr
 
-from antlr4 import FileStream
+from antlr4 import FileStream, InputStream
 
 from setlx2python.transpiler import Transpiler, NotSupported, parse_input
 from . import __version__ as VERSION
 
 
-def transpile(file):
-    input = FileStream(file, encoding="utf-8")
+def transpile(file=None, code=None):
+    if code != None:
+        input = InputStream(code)
+    elif file != None:
+        input = FileStream(file, encoding="utf-8")
+    else:
+        raise Exception("no file or code provided")
     parser = parse_input(input)
     tree = parser.block()
     t = Transpiler(tree.blk)
@@ -53,5 +60,9 @@ def pretty_source(source):
 
 def main():
     options = docopt(__doc__, version=VERSION)
-    ast_tree = transpile(options["<file>"])
+    if options["-c"]:
+        ast_tree = transpile(code=options["<file>"])
+    else:
+        ast_tree = transpile(file=options["<file>"])
+
     print(to_source(ast_tree, pretty_source=pretty_source))
